55c86dfd87a3e7f4675a0a71cdacf027609ccf83
itegoss2025 <sushil@itegoss.in>
2025-03-18 15:41:23 +0530

    backend is completed.

--- app.js
+++ app.js
@@ -6,7 +6,39 @@ import cors from "cors";
 const app = express();
 const PORT = 4000;
 
-app.use(cors({ origin: "http://localhost:5173" }));
+// const allowedOrigins = [
+//   "http://localhost:5173",
+// ];
+
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       // Allow requests with no origin (like mobile apps or curl)
+//       if (!origin) return callback(null, true);
+//       if (allowedOrigins.includes(origin)) {
+//         return callback(null, true);
+//       } else {
+//         return callback(new Error("Not allowed by CORS"));
+//       }
+//     },
+//   })
+// );
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://fileuploader-itegoss.netlify.app",
+];
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (allowedOrigins.includes(origin) || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  })
+);
 
 app.use(express.json());
 
@@ -15,5 +47,3 @@ app.use("/api", router);
 app.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}`);
 });
-
-
