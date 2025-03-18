9224a92ad04eeaa6abe9a1ae51f67db600c53065
itegoss2025 <sushil@itegoss.in>
2025-03-18 11:49:59 +0530

    changes are made in the db.js

--- db.cjs
+++ db.cjs
@@ -8,7 +8,7 @@ const client = new Client({
   database: process.env.PG_DATABASE,
   password: process.env.PG_PASSWORD,
   port: process.env.PG_PORT || 5432,
-  ssl: process.env.PG_SSL === "true" ? { rejectUnauthorized: false } : false,
+  // ssl: process.env.PG_SSL === "true" ? { rejectUnauthorized: false } : false,
 });
 
 // client.query("SELECT NOW();")
